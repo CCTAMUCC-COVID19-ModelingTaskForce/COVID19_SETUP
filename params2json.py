a00cd11fcb43fbb2db2c45dab8bf072e7968d192
Evan Krell <evan.krell@tamucc.edu>
2020-07-20 17:04:10 -0500

    Update params2json.py

--- params2json.py
+++ params2json.py
@@ -85,11 +85,11 @@ parameters["scenarioData"]["data"]["population"]["populationServed"] = int(param
 parameters["scenarioData"]["data"]["simulation"]["numberStochasticRuns"] = int(paramsDF.loc[paramsDF["parameter"] == "Number of Runs", "meanVal"].item())
 start = paramsDF.loc[paramsDF["parameter"] == "Simulation start date", "meanVal"].item().split("/")
 startDate = datetime.datetime(int(start[2]), int(start[0]), int(start[1]), 0, 0, 0)
-startDate += datetime.timedelta(days = 1)
+#startDate += datetime.timedelta(days = 1)
 start = "{}-{:02d}-{:02d}T00:00:00.000Z".format(startDate.year, startDate.month, startDate.day)
 stop = paramsDF.loc[paramsDF["parameter"] == "Simulation end date", "meanVal"].item().split("/")
 stopDate = datetime.datetime(int(stop[2]), int(stop[0]), int(stop[1]), 0, 0, 0)
-stopDate += datetime.timedelta(days = 1)
+#stopDate += datetime.timedelta(days = 1)
 stop = "{}-{:02d}-{:02d}T00:00:00.000Z".format(stopDate.year, stopDate.month, stopDate.day)
 parameters["scenarioData"]["data"]["simulation"]["simulationTimeRange"]["begin"] = start
 parameters["scenarioData"]["data"]["simulation"]["simulationTimeRange"]["end"] = stop
@@ -124,11 +124,11 @@ for name, lower, upper, start, stop in zip(mitigationsDF["Intervention"], \
 
     start = start.split("/")
     startDate = datetime.datetime(int(start[2]), int(start[0]), int(start[1]), 0, 0, 0)
-    startDate += datetime.timedelta(days = 1)
+    #startDate += datetime.timedelta(days = 1)
     start = "{}-{:02d}-{:02d}T17:00:00.000Z".format(startDate.year, startDate.month, startDate.day)
     stop = stop.split("/")
     stopDate = datetime.datetime(int(stop[2]), int(stop[0]), int(stop[1]), 0, 0, 0)
-    stopDate += datetime.timedelta(days = 1)
+    #stopDate += datetime.timedelta(days = 1)
     stop = "{}-{:02d}-{:02d}T17:00:00.000Z".format(stopDate.year, stopDate.month, stopDate.day)
 
     mitigations.append({
