e87e3569a65f5cc8a4acf085a5d7b92560e56002
Chris Bird <cbird808@gmail.com>
2020-07-20 14:32:14 -0500

    updated TSA-V

--- tsa_scripts.bash
+++ tsa_scripts.bash
@@ -1,8 +1,9 @@
 # code to make a set of tsv files for a new TSA using an exsisting TSA's files, then convert to json
 
 ## load variables
+paramsPATH=./params
 oldTSA=V
-newTSA=U
+newTSA=V
 
 ## make lists of old and new file names
 ls TSA-${oldTSA}*tsv > fileNames_TSA-${oldTSA}.txt
@@ -15,6 +16,6 @@ parallel --link "cp {1} {2}" :::: fileNames_TSA-${oldTSA}.txt fileNames_TSA-${ne
 sed "s/TSA-$oldTSA/TSA-$newTSA/g" TSA-${newTSA}_params.tsv
 
 ## make json
-python3 params2json.py -j TSA-${newTSA}_fit.json -p TSA-${newTSA}_params.tsv -a TSA-${newTSA}_popByAge.tsv -m TSA-${newTSA}_mitigations.tsv -s TSA-${newTSA}_severity.tsv
+python3 params2json.py -j $paramsPATH/TSA-${newTSA}_fit.json -p $paramsPATH/TSA-${newTSA}_params.tsv -a $paramsPATH/TSA-${newTSA}_popByAge.tsv -m $paramsPATH/TSA-${newTSA}_mitigations.tsv -s $paramsPATH/TSA-${newTSA}_severity.tsv
 
 
